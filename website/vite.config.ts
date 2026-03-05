1d52a0ff85660d75f9a09b06d11c5e5e360b3c20
Ripardo <ripardoo@users.noreply.github.com>
2026-03-05 19:33:10 +0200

    Updated config files.

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
+import { cloudflare } from '@cloudflare/vite-plugin'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
 import viteReact from '@vitejs/plugin-react'
@@ -10,6 +10,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     devtools(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
